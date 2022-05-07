86959f41365a2ec4628ef62ee3e66142b7d93908
linhphandk <thachlinhphan@gmail.com>
2022-05-07 09:15:19 +0200

    [ADD]: cors

--- static-web-server/app.js
+++ static-web-server/app.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import path from 'path'
+import cors from 'cors'
 const app = express()
 const ASSETS_FOLDER=path.join(process.cwd(),'assets')
 const PORT = 8888
+
+app.use(cors())
 app.use(express.static(ASSETS_FOLDER));
 
 app.listen(PORT, ()=>{
